457ecc6de0fffae10c2eed88b2939a7482fed30a
Lucas Malinski <mailpatriota@gmail.com>
2025-09-08 15:35:51 -0300

    Adicionadas tarefas à DAG

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# banvic_indicium
-Exercício de engenharia de dados para processo seletivo do programa indicium lighthouse. 
+Para o exercício, o docker-compose fornecido pela documentação do airflow para ambientes de produção em docker seria desnecessariamente complexo. 
+
+Alguns serviços são dispensáveis em desenvolvimento local, ou quando não usando o CeleryExecutor.
+
+1. **Redis**
+2. **Airflow Worker**
+3. **Airflow Triggerer**
+4. **Airflow DAG Processor**
+5. **Flower**
+
+Apesar dos serviços suprimidos, mantive o arquivo fornecido por: 
+    1. Manter a arquitetura sugerida
+    2. Preservar mensagens de erro relacionadas à inicialização do airflow definidas no arquivo.
+
---
+++ airflow/dags/banvic_dag.py
@@ -0,0 +1,53 @@
+import pendulum
+from datetime import timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow import DAG
+
+
+# [VARIÁVEIS GLOBAIS DA DAG]
+# Padrão em todos operadores caso variáveis não sejam sobrescritas na definição da tarefa.
+
+
+default_args ={
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5) 
+}
+with DAG(
+    "banvic_pipeline",
+    default_args = default_args,
+    start_date = pendulum.datetime(2025, 9, 10, 4, 35),
+    schedule="35 4 * * *",  # diariamente 04:35
+    catchup = False,
+) as dag: 
+    
+
+
+# [TASKS]
+
+    extrair_csv = DockerOperator(
+        task_id = "extrair_csv",
+        image="meltano/meltano:latest",
+        command="meltano run tap-csv target-local-csv",
+        auto_remove=True,
+
+    )
+
+    extrair_sql = DockerOperator(
+        task_id = "extrair_sql",
+        image="meltano/meltano:latest",
+        command="meltano run tap-postgres target-local-csv",
+        auto_remove=True,
+
+    )
+
+    carregar_dw = DockerOperator(
+        task_id = "carregar_dw",
+        image="meltano/meltano:latest",
+        command="meltano run target-local-csv target-postgres",
+        auto_remove=True,
+
+    )
+
+    # Garante que 'carregar_dw' só será executada após 'extrair_csv' e 'extrair_sql' serem concluídas
+    [extrair_csv, extrair_sql] >> carregar_dw
