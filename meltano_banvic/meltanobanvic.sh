89c11f547f79f9ed4ba79a715e5f4d98552584b8
Lucas Malinski <mailpatriota@gmail.com>
2025-09-10 13:56:09 -0300

    id de grupo e usuario para evitar meltano root

--- meltano_banvic/meltanobanvic.sh
+++ meltano_banvic/meltanobanvic.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
 
+# Definir id de grupo e usuário para evitar que meltano rode como root
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
 # Helper script utilizando o meltano através da imagem meltano-banvic já built. 
 docker compose run --rm meltano "$@"
