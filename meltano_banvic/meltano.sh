a2f941f76ac927452a66ea42c36264a573f96c08
Lucas Malinski <mailpatriota@gmail.com>
2025-09-10 11:23:22 -0300

    .

--- meltano_banvic/meltano.sh
+++ meltano_banvic/meltano.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# Helper script utilizando o meltano através da imagem meltano/meltano:latest para configurar o meltano antes da construção da imagem docker 
+
 docker run \
     -u $(id -u):$(id -g) \
     -e HOME=/projects \
