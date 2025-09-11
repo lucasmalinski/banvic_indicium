8af2c47c2b763e330f5a01e55a4d20c9dc9994ff
Lucas Malinski <mailpatriota@gmail.com>
2025-09-10 22:42:58 -0300

    .

--- meltano_banvic/meltanobanvic.sh
+++ meltano_banvic/meltanobanvic.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Definir id de grupo e usuário para evitar que meltano rode como root
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
+#export USER_ID=$(id -u)
+#export GROUP_ID=$(id -g)
 
 # Helper script utilizando o meltano através da imagem meltano-banvic já built. 
 docker compose run --rm meltano "$@"
