9431f7c6de49e47e7ce724b8adb5d6a4bfcbbc8e
Lucas Malinski <mailpatriota@gmail.com>
2025-09-12 01:37:57 -0300

    .

--- pipeline.sh
+++ pipeline.sh
@@ -4,16 +4,12 @@ set -e
 # Permissões de pastas e environment do airflow
 
 echo -e "AIRFLOW_UID=$(id -u)" > airflow/.env
-echo -e "AIRFLOW_UID=$(id -u)" > ./.env
-mkdir -p airflow/dags airflow/logs airflow/plugins airflow/config
-sudo chown -R "$(id -u):$(id -u)" airflow/dags airflow/logs airflow/plugins airflow/config
-sudo chmod -R 755 airflow/dags airflow/logs airflow/plugins airflow/config
 
-BUILD imagem meltano pré-configurada 
+# BUILD imagem meltano pré-configurada 
 
 docker build -t "meltano-banvic:latest" "meltano_banvic"
 
-POSTGRES do Banvic
+# POSTGRES do Banvic
 
 docker compose -f postgres_banvic/docker-compose.yml up -d 
 
@@ -22,4 +18,4 @@ docker compose -f data_warehouse/docker-compose.yml up -d
 
 # AIRFLOW
 cd airflow
-docker compose up airflow-init --force-recreate && docker compose up -d  --force-recreate
+docker compose up airflow-init --force-recreate && docker compose up -d --force-recreate
